3f056c1ac61d0a11befc786ba6841aa5687fc637
Jens Somers <somersjens@gmail.com>
2026-01-30 21:25:29 +0100

    adjusted hex

--- Traditional Chef/Traditional Chef/AppTheme.swift
+++ Traditional Chef/Traditional Chef/AppTheme.swift
@@ -7,11 +7,11 @@ import SwiftUI
 
 enum AppTheme {
     static let primaryBlue = Color(hex: "1C263C")
-    static let secondaryOffWhite = Color(hex: "FBF5F0")
-    static let pageBackground = Color(hex: "FBF5F0")
-    static let cardBackground = Color.white
-    static let searchBarBackground = Color(hex: "FFF9F4")
+    static let secondaryOffWhite = Color(hex: "FEF7F0")
+    static let pageBackground = Color(hex: "FEF7F0")
+    static let cardBackground = Color(hex: "FFFAF7")
+    static let searchBarBackground = Color(hex: "FFFAF7")
     static let textPrimary = Color(hex: "1C263C")
     static let searchHighlight = Color(hex: "1C263C")
-    static let hairline = Color(hex: "D5DDE7")
+    static let hairline = Color(hex: "1C263C")
 }
