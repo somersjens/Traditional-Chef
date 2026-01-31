67fc8cbb4e731a3de8f85caca637d1b2106e04f9
Jens Somers <somersjens@gmail.com>
2026-01-31 14:13:39 +0100

    lighter yellow intrpduced

--- Traditional Chef/Traditional Chef/AppTheme.swift
+++ Traditional Chef/Traditional Chef/AppTheme.swift
@@ -7,10 +7,10 @@ import SwiftUI
 
 enum AppTheme {
     static let primaryBlue = Color(hex: "1C263C")
-    static let secondaryOffWhite = Color(hex: "FEF7F0")
-    static let pageBackground = Color(hex: "FEF7F0")
-    static let cardBackground = Color(hex: "FFFAF7")
-    static let searchBarBackground = Color(hex: "FFFAF7")
+    static let secondaryOffWhite = Color(hex: "FAF5F0")
+    static let pageBackground = Color(hex: "FAF5F0")
+    static let cardBackground = Color(hex: "FEFCFB")
+    static let searchBarBackground = Color(hex: "FEFCFB")
     static let textPrimary = Color(hex: "1C263C")
     static let searchHighlight = Color(hex: "1C263C")
     static let hairline = Color(hex: "1C263C")
