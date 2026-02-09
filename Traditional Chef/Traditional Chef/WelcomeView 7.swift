35904e58100da1277438391ae8a7dd2af60e117c
Jens Somers <somersjens@gmail.com>
2026-02-09 17:02:10 +0100

    slightly

--- Traditional Chef/Traditional Chef/WelcomeView 7.swift
+++ Traditional Chef/Traditional Chef/WelcomeView 7.swift
@@ -57,6 +57,7 @@ struct WelcomeView: View {
                 Spacer(minLength: verticalSizeClass == .compact ? 0 : 12)
             }
             .padding(.top, 8)
+            .scaleEffect(verticalSizeClass == .compact ? 1.0 : 1.05, anchor: .top)
         }
         .task {
             await runAnimationLoop()
